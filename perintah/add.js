0d9ea48a00e91d9aeafb89e44a918bf1aaf5c0e1
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-19 14:02:09 +0700

    Update add.js

--- perintah/add.js
+++ perintah/add.js
@@ -5,7 +5,11 @@ async function Alya(api, event) {
   const text = event.body?.replace(":add", "")?.trim().toLowerCase();
 
   if (text) {
+     try { 
       api.addUserToGroup(text, event.threadID, event.messageID);
+     } catch (e) {
+       console.log(e);
+     }
   } else {
     return api.sendMessage("Masukkan id nya bodo", event.threadID, event.messageID);
   }
