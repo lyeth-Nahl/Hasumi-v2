e75b5f4113678b5fc56c27aef2a2e12a4245bc83
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-19 14:31:38 +0700

    Update meme.js

--- perintah/meme.js
+++ perintah/meme.js
@@ -8,7 +8,12 @@ module.exports = {
        const axios = require('axios');
         const meme = await axios.get("https://raw.githubusercontent.com/HadyZen/hady-zen-api/refs/heads/main/meme.json");
         const itsuki = meme.data;
-  const foto = itsuki[Math.floor(Math.random() * itsuki.length)];
+  const ft = itsuki[Math.floor(Math.random() * itsuki.length)];
+       const response = await axios.get(ft, {
+      responseType: 'arraybuffer', 
+    });
+
+    const foto = response.data;
   api.sendMessage({ attachment:  foto }, event.threadID, event.messageID);
 } catch (futaro) { 
   console.log(futaro);
