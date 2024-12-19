c36fb085e7cfcadc4243da0b2d03af51b30bff32
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-19 22:07:55 +0700

    Update add.js

--- perintah/add.js
+++ perintah/add.js
@@ -6,7 +6,7 @@ async function Alya(api, event) {
 
   if (text) {
      try { 
-      api.addUserToGroup(text, event.threadID, event.messageID);
+      api.addUserToGroup(text, event.threadID);
      } catch (e) {
        console.log(e);
      }
