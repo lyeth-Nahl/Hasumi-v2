d1a4fd3c01b970fbcc73b6ca802c1c13bb89aec5
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-20 18:20:13 +0700

    Update flux.js

--- perintah/flux.js
+++ perintah/flux.js
@@ -1,13 +1,11 @@
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-
 module.exports = {
-    config: {
-  nama: "flux"
-}
+    config: { nama: "flux" }, 
+        
     Alya: async function(api, event) {
         try {
+            const axios = require('axios');
+            const fs = require('fs');
+            const path = require('path');
             const prompt = event.body?.replace(":flux", "")?.trim().toLowerCase();
             const response = await axios.get(`https://kaiz-apis.gleeze.com/api/flux-1.1-pro?prompt=${encodeURIComponent(prompt)}`, {
                 responseType: 'arraybuffer'
