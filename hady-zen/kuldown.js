903f0ad15086dafc3d884d3e4ba54ef0d516a676
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-20 22:45:32 +0700

    Create kuldown.js

---
+++ hady-zen/kuldown.js
@@ -0,0 +1,24 @@
+let hadi = {};
+
+function kuldown(id, cmd, cd) {
+    if (jeda(id, cmd, cd)) {
+        if (!hadi[id]) {
+            hadi[id] = {};
+        }
+        hadi[id][cmd] = Date.now();
+        return "hadi";
+    } else {
+        return "pipi";
+    }
+}
+
+function jeda(id, cmd, cd) {
+    if (!hadi[id] || !hadi[id][cmd]) {
+        return true;
+    }
+    const timePassed = (Date.now() - hadi[id][cmd]) / 1000; 
+
+    return timePassed >= cd;
+}
+
+module.exports = { kuldown };
