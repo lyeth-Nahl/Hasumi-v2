7d6a750b1f724d5b3b80fd02ab78abdab3fc012d
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-20 22:59:41 +0700

    Update uid.js

--- perintah/uid.js
+++ perintah/uid.js
@@ -1,5 +1,8 @@
 module.exports = {
-  config: { nama: "uid" }, 
+  config: { 
+    nama: "uid", 
+    kuldown: 10
+  }, 
   Alya: async function(api, event) {
     api.sendMessage(event.senderID, event.threadID, event.messageID)
   }
