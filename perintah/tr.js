ee8cb78b6bbbd7514dac46eb616d1c1b833228f9
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-21 16:36:20 +0700

    Update tr.js

--- perintah/tr.js
+++ perintah/tr.js
@@ -13,7 +13,7 @@ async function Alya(api, event) {
       const hady = parts.slice(1).join(' ');
       const respon = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${bhs}&dt=t&q=${encodeURIComponent(hady)}`);
       if (respon.data[0].map(item => item[0]).join('')) {
-        return api.sendMessage(respon.data[0].map(item => item[0]).join(''), event.threadID, event.messageID);
+        return api.sendMessage(`# Terjemahan\n\n${respon.data[0].map(item => item[0]).join('')}`, event.threadID, event.messageID);
       } else {
         return api.sendMessage("gak di jawab", event.threadID, event.messageID);
       }
