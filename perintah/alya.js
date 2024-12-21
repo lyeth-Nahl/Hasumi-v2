6f1048bc13939fcc6b0ff0ae1a38d0442a818d31
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-21 21:26:42 +0700

    Update alya.js

--- perintah/alya.js
+++ perintah/alya.js
@@ -1,22 +1,25 @@
 const config = { 
   nama: "alya",
-  kuldown: 10
+  penulis: "Hady Zen", 
+  kuldown: 10,
+  role: 0,
+  tutor: "<pertanyaan>"
 };
 
-async function Alya(api, message) { 
+async function Alya(api, event, args) { 
   const axios = require('axios');
-  const text = message.body?.replace(":alya", "")?.trim().toLowerCase();
+  const text = args.join(' ');
 
   if (text) {
       const hady = `nama kamu adalah Alya Kujou. User input: ${text}`;
       const respon = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(hady)}`);
       if (respon.data.answer) {
-        return api.sendMessage(respon.data.answer, message.threadID, message.messageID);
+        return api.sendMessage(respon.data.answer, event.threadID, event.messageID);
       } else {
-        return api.sendMessage("gak di jawab", message.threadID, message.messageID);
+        return api.sendMessage("gak di jawab", event.threadID, event.messageID);
       }
   } else {
-    return api.sendMessage("Masukkan pesan nya bodo", message.threadID, message.messageID);
+    return api.sendMessage("Masukkan pesan nya bodo", event.threadID, event.messageID);
   }
 }
 module.exports = { config, Alya };
