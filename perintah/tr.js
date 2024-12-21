79cb331f6144aa64a555a7a66c8bd430ee445c2b
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-21 16:34:29 +0700

    Update tr.js

--- perintah/tr.js
+++ perintah/tr.js
@@ -6,10 +6,11 @@ const config = {
 async function Alya(api, event) { 
   const axios = require('axios');
   const text = event.body?.replace(":tr", "")?.trim().toLowerCase();
+  const parts = text?.split(' ');
 
-  if (text) {
-      const hady = text[1].join(' ');
-      const bhs = text[0];
+  if (parts && parts.length >= 2) {
+      const bhs = parts[0];
+      const hady = parts.slice(1).join(' ');
       const respon = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${bhs}&dt=t&q=${encodeURIComponent(hady)}`);
       if (respon.data[0].map(item => item[0]).join('')) {
         return api.sendMessage(respon.data[0].map(item => item[0]).join(''), event.threadID, event.messageID);
