bb17e3fa3f510558029d4df7f4d47d2bd5a17c3b
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-22 22:19:04 +0700

    Create file.js

---
+++ perintah/file.js
@@ -0,0 +1,21 @@
+module.exports = {
+  config: { 
+    nama: "file", 
+    penulis: "Hady Zen", 
+    peran: 2,
+    kuldown: 10,
+    tutor: "<nama file>"
+  }, 
+  Alya: async function(api, event, args) {
+    const fs = require('fs');  
+    const path = require('path'); 
+    if (!args) return api.sendMessage('Berikan nama file nya su.', event.threadID, event.messageID);
+    const filePath = path.join('perintah', `${args[0]}.js`);
+    const file = fs.readFileSync(filePath, 'utf8');
+    if (!file || file.length < 0) { 
+      api.sendMessage('Gada file itu wak', event.threadID, event.messageID);
+    } else { 
+      api.sendMessage(file, event.threadID, event.messageID);  
+    }
+  }
+};
