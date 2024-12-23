78a582edd1c6cdda2b9e26b3720d94a93abbdb5e
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-23 18:47:29 +0700

    Update eval.js

--- perintah/eval.js
+++ perintah/eval.js
@@ -7,46 +7,10 @@ module.exports = {
   },
 
   Alya: async function ({ api, event, args }) {
-    function output(msg) {
-      if (typeof msg === "number" || typeof msg === "boolean" || typeof msg === "function") {
-        msg = msg.toString();
-      } else if (msg instanceof Map) {
-        let text = `Map(${msg.size}) `;
-        text += JSON.stringify(mapToObj(msg), null, 2);
-        msg = text;
-      } else if (typeof msg === "object") {
-        msg = JSON.stringify(msg, null, 2);
-      } else if (typeof msg === "undefined") {
-        msg = "undefined";
-      }
-
-      api.sendMessage(msg, event.threadID); 
-    }
-
-    // Helper function for converting Map to Object
-    function mapToObj(map) {
-      const obj = {};
-      map.forEach((v, k) => {
-        obj[k] = v;
-      });
-      return obj;
-    }
-
-    // Join the arguments for eval
-    const cmd = `
-    (async () => {
-      try {
-        let result = ${args.join(" ")}; 
-        output(result);
-      } catch (err) {
-        api.sendMessage("An error occurred while executing eval: " + err.message, event.threadID);
-      }
-    })()`;
-
-    try {
-      eval(cmd); 
-    } catch (err) {
-      api.sendMessage("Failed to run eval: " + err.message, event.threadID);
-    }
+  try { 
+    eval(args.join(" "));
+  } catch (p) {
+    api.sendMessage("Error: " + p.message, event.threadID, event.messageID);
   }
+ }
 };
