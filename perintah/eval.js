7ec00de4a33b1e8c4f76be0c19a00af1881e7b9d
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-23 19:01:49 +0700

    Update eval.js

--- perintah/eval.js
+++ perintah/eval.js
@@ -6,27 +6,103 @@ module.exports = {
     peran: 2,
   },
 
-  Alya: async function ({ api, event, args }) {
-    // Pastikan ada argumen untuk dieksekusi
-    if (!args || args.length === 0) {
-      return api.sendMessage("Harap masukkan kode yang akan dieksekusi.", event.threadID, event.messageID);
+  Alya: async function(api, event, args) {
+
+        function output(msg) {
+
+            if (typeof msg === 'number' || typeof msg === 'boolean' || typeof msg === 'function') {
+
+                msg = msg.toString();
+
+            } else if (msg instanceof Map) {
+
+                let text = `Map(${msg.size}) `;
+
+                text += JSON.stringify(mapToObj(msg), null, 2);
+
+                msg = text;
+
+            } else if (typeof msg === 'object') {
+
+                msg = JSON.stringify(msg, null, 2);
+
+            } else if (typeof msg === 'undefined') {
+
+                msg = 'undefined';
+
+            }
+
+            api.sendMessage(msg, event.threadID);
+
+        }
+
+
+
+        function mapToObj(map) {
+
+            const obj = {};
+
+            map.forEach(function(v, k) {
+
+                obj[k] = v;
+
+            });
+
+            return obj;
+
+        }
+
+
+
+        try {
+
+            if (!args[0]) {
+
+                return api.sendMessage("❌ Mohon masukkan kode yang akan dijalankan!", event.threadID);
+
+            }
+
+
+
+            const cmd = `
+
+            (async () => {
+
+                try {
+
+                    ${args.join(" ")}
+
+                }
+
+                catch(err) {
+
+                    console.error("eval command", err);
+
+                    api.sendMessage(
+
+                        "❌ Error:\\n" +
+
+                        (err.stack ? err.stack.toString() : JSON.stringify(err, null, 2))
+
+                    , event.threadID);
+
+                }
+
+            })()`;
+
+
+
+            eval(cmd);
+
+        } catch (error) {
+
+            api.sendMessage(`❌ Error:
+
+Input: ${args.join(" ")}
+Error: ${error.message}`, event.threadID);
+
+        }
+
     }
 
-    try {
-      // Gabungkan argumen menjadi satu string dan jalankan dengan eval
-      const code = args.join(" ");
-      let result = eval(code); // Hati-hati menggunakan eval
-
-      // Jika hasil eval adalah objek atau array, kita bisa mengubahnya menjadi string
-      if (typeof result === "object") {
-        result = JSON.stringify(result, null, 2);
-      }
-
-      // Kirimkan hasil eksekusi kode ke pengguna
-      api.sendMessage(`Hasil:\n${result}`, event.threadID, event.messageID);
-    } catch (error) {
-      // Tangani error yang terjadi saat eksekusi eval
-      api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
-    }
-  }
 };
