3ece2205d248d872023737e80d4efc549dd420d7
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-23 13:54:33 +0700

    Create tes.js

---
+++ perintah/tes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  config: { 
+    nama: "tes", 
+    kuldown: 1
+  }, 
+  Alya: async function(api, event) {
+    const { adminIDs } = api.getThreadInfo(event.threadID);
+    api.sendMessage(adminIDs, event.threadID);
+};
