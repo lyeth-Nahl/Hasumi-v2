3ddd6379773fa43f719b62227fc8ef5988b0fedf
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-23 16:23:28 +0700

    Update tes.js

--- perintah/tes.js
+++ perintah/tes.js
@@ -4,7 +4,19 @@ module.exports = {
     kuldown: 1
   }, 
   Alya: async function(api, event) {
-    const p = api.getThreadInfo(event.threadID);
-    api.sendMessage(`Nih: ${p.adminIDs}`, event.threadID);
+    // Mengambil informasi thread secara asinkron
+const threadInfo = await new Promise((resolve, reject) => {
+    api.getThreadInfo(event.threadID, (err, info) => {
+        if (err) return reject(err); // Menangani error
+        resolve(info); // Mengembalikan data jika sukses
+    });
+});
+
+// Mengambil daftar admin dari informasi thread
+const adminList = threadInfo.adminIDs;
+
+
+const message = `${adminList.join("\n\n")}`;
+api.sendMessage(message, event.threadID);
   }
 };
