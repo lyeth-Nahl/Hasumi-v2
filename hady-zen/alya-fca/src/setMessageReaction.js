f9e1de8dc58a9f10bf307b37990af1d680d23154
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-23 17:00:00 +0700

    Update setMessageReaction.js

--- hady-zen/alya-fca/src/setMessageReaction.js
+++ hady-zen/alya-fca/src/setMessageReaction.js
@@ -25,58 +25,6 @@ module.exports = function (defaultFuncs, api, ctx) {
 			throw { error: "Cannot set reaction as another profile." };
 		}
 
-		switch (reaction) {
-			case "\uD83D\uDE0D": //:heart_eyes:
-			case "\uD83D\uDE06": //:laughing:
-			case "\uD83D\uDE2E": //:open_mouth:
-			case "\uD83D\uDE22": //:cry:
-			case "\uD83D\uDE20": //:angry:
-			case "\uD83D\uDC4D": //:thumbsup:
-			case "\uD83D\uDC4E": //:thumbsdown:
-			case "\u2764": //:heart:
-			case "\uD83D\uDC97": //:glowingheart:
-			case "":
-				//valid
-				break;
-			case ":heart_eyes:":
-			case ":love:":
-				reaction = "\uD83D\uDE0D";
-				break;
-			case ":laughing:":
-			case ":haha:":
-				reaction = "\uD83D\uDE06";
-				break;
-			case ":open_mouth:":
-			case ":wow:":
-				reaction = "\uD83D\uDE2E";
-				break;
-			case ":cry:":
-			case ":sad:":
-				reaction = "\uD83D\uDE22";
-				break;
-			case ":angry:":
-				reaction = "\uD83D\uDE20";
-				break;
-			case ":thumbsup:":
-			case ":like:":
-				reaction = "\uD83D\uDC4D";
-				break;
-			case ":thumbsdown:":
-			case ":dislike:":
-				reaction = "\uD83D\uDC4E";
-				break;
-			case ":heart:":
-				reaction = "\u2764";
-				break;
-			case ":glowingheart:":
-				reaction = "\uD83D\uDC97";
-				break;
-			default:
-				if (forceCustomReaction) {
-					break;
-				}
-				return callback({ error: "Reaction is not a valid emoji." });
-		}
 
 		const variables = {
 			data: {
