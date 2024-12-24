4420cdebf98cd84a94c7ba2a2915f6702b4bb522
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-24 12:18:19 +0700

    Update uid.js

--- perintah/uid.js
+++ perintah/uid.js
@@ -3,7 +3,11 @@ module.exports = {
     nama: "uid", 
     kuldown: 10
   }, 
-  Alya: async function(api, event) {
-    api.sendMessage(event.senderID, event.threadID, event.messageID)
+  Alya: async function(api, event, args) {
+    api.sendMessage(event.senderID, event.threadID, event.messageID);
+    if (args[0]) {
+      const p = Object.keys(event.mentions);
+          api.sendMessage(p, event.threadID, event.messageID);
+    }
   }
 };
