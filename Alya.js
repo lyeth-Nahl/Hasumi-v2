e0e6c665110c3de0d7e31d2ebc1c33bc5d23bbdb
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-24 19:19:15 +0700

    Update Alya.js

--- Alya.js
+++ Alya.js
@@ -1,52 +1,46 @@
-        const express = require('express');
-	const app = express();
-	const axios = require('axios');
-	const login = require("./hady-zen/alya-fca");
-	const { warna, font, logo } = require("./hady-zen/log.js");
-	const fs = require("fs");
-	const path = require("path");
-	const akun = fs.readFileSync('akun.txt', 'utf8');
-	const { awalan, nama, admin, proxy, port } = require('./config.json');
-        const { kuldown } = require('./hady-zen/kuldown');
+/* HADY ZEN'IN */
+
+ const express = require('express');
+ const app = express();
+ const axios = require('axios');
+ const login = require("./hady-zen/alya-fca");
+ const { warna, font, logo } = require("./hady-zen/log.js");
+ const fs = require("fs");
+ const path = require("path");
+ const akun = fs.readFileSync('akun.txt', 'utf8');
+ const { awalan, nama, admin, proxy, port } = require('./config.json');
+ const { kuldown } = require('./hady-zen/kuldown');
 
 console.log(warna.biru + `▄▀█ █░ █▄█ ▄▀█  █▄▀ █░█ ░█ █▀█ █░█\n█▀█ █▄ ░█░ █▀█  █░█ █▄█ ▄█ █▄█ █▄█\n`);
 console.log(logo.info + "Chatbot messenger by hady and saveng.");
-	if (!akun || akun.length < 0) {
-console.log(logo.error + 'Harap masukkan cookie terlebih dahulu.');
-	}
 
+if (!akun || akun.length < 0) return console.log(logo.error + 'Harap masukkan cookie terlebih dahulu.');
 const zen = { host: proxy, port: port };
-login({appState: JSON.parse(fs.readFileSync('akun.txt', 'utf8'), zen)}, (err, api) => {
-		if(err) return console.log(logo.error + `terjadi kesalahan saat login: ${err.message}`);
-	api.setOptions({listenEvents: true});
+login({appState: JSON.parse(akun, zen)}, (err, api) => {
+   if(err) return console.log(logo.error + `Terjadi kesalahan saat login: ${err.message}`);
+	
+   api.setOptions({listenEvents: true});
 console.log(logo.login + 'Mulai menerima pesan dari pengguna.');
 	  
-		api.listenMqtt((err, event) => {
-            const body = event.body;
-	    if (!body) return;
-            if (body.toLowerCase() == "prefix") return api.sendMessage(`✨ Awalan ${nama} adalah: [ ${awalan} ]`, event.threadID, event.messageID);
-            if (!body.startsWith(awalan) || body == " ") return console.log(logo.pesan + `${event.senderID} > ${body}`);
-                const saveng = body.slice(awalan.length).trim().split(/ +/g);
-                const cmd = saveng.shift().toLowerCase();
+   api.listenMqtt((err, event) => {
+   const body = event.body;
+if (!body) return;
+if (body.toLowerCase() == "prefix") return api.sendMessage(`✨ Awalan ${nama} adalah: [ ${awalan} ]`, event.threadID, event.messageID);
+if (!body.startsWith(awalan) || body == " ") return console.log(logo.pesan + `${event.senderID} > ${body}`);
+        const saveng = body.slice(awalan.length).trim().split(/ +/g);
+        const cmd = saveng.shift().toLowerCase();
+			
             async function hady_cmd(cmd, api, event) {
-		const pipi = body?.replace(`${awalan}${cmd}`, "")?.trim().toLowerCase();
-                const args = pipi?.split(' ');
-                const folder = path.join(__dirname, '/perintah');
-		const skibidi = await new Promise((resolve, reject) => { api.getThreadInfo(event.threadID, (err, info) => {
-            if (err) reject(err);
-            else resolve(info);
-        });
-});
-
-    const hooh = skibidi.adminIDs;
-    const fitri = hooh.map(admin => admin.id);
-    const ff = fitri.join(", ");
-
-         try {
-       const files = fs.readdirSync(folder);
+       const pipi = body?.replace(`${awalan}${cmd}`, "")?.trim().toLowerCase();
+       const args = pipi?.split(' ');
+	 try { 
+       const skibidi = await new Promise((resolve, reject) => { api.getThreadInfo(event.threadID, (err, info) => { if (err) reject(err); else resolve(info); }); });
+       const fitri = skibidi.adminIDs.map(admin => admin.id);
+       const ff = fitri.join(", ");
+       const files = fs.readdirSync(path.join(__dirname, '/perintah'));
        for (const file of files) {
    if (file.endsWith('.js')) {
-       const anime = path.join(folder, file);
+       const anime = path.join(path.join(__dirname, '/perintah'), file);
        const { config, Alya } = require(anime);
 
    if (config && config.nama === cmd && typeof Alya === 'function') {
@@ -71,10 +65,7 @@ if ((config.peran == 2 || config.peran == 1) && admin.includes(event.senderID) |
   } else {
    api.setMessageReaction('⌛', event.messageID);
   } 
-  } else { 
-   api.setMessageReaction('❔', event.messageID);
-   }
-  }
+ }
 }
  } catch (error) {
    console.log(logo.error + 'Perintah error: ' + error.message);
