2ba54debfe484007f32d4b3571b9b958b5ef4a11
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2024-12-28 11:13:02 +0700

    Hady Zen

--- stop.js
+++ stop.js
@@ -1,10 +1,10 @@
 /* HADY ZEN'IN */
 
 function stop() {
+  console.clear();
   const { logo } = require('./hady-zen/log');
-console.clear();
   console.log(logo.info + "Alya telah diperintahkan untuk berhenti.");
-process.exit(1);
-}
+  process.exit(1);
+};
 
 stop();
