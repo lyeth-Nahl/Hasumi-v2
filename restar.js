8cc6bcb0168c371ddd87eccd887d1a0a9700af04
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-01 14:06:38 +0700

    Hady Zen

--- restar.js
+++ restar.js
@@ -1,12 +1,8 @@
 /* HADY ZEN'IN */
 
-const hady = require('child_process');
 const { logo } = require('./hady-zen/log');
 
-function restar() {
-  console.log(logo.info + 'Alya akan dimulai ulang...');
-  hady.exec('npm start');
+process.on('SIGINT', function() {
+  console.log(logo.info + 'Alya diperintahkan berhenti...');
   process.exit();
-}
-
-restar();
+});
