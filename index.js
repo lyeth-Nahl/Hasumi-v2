46419a7501428a14ebbb12e7f1196e823a4abf08
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-01 22:03:28 +0700

    Hady Zen

--- index.js
+++ index.js
@@ -3,18 +3,17 @@
 const { spawn } = require("child_process");
 
 function hady() {
-  const child = spawn("npm start", {
+  const child = spawn("node Alya.js", {
     cwd: __dirname,
     stdio: "inherit",
     shell: true
   });
 
   child.on("close", (code) => {
-    if (code == 2) {
-      hady();
-    }
+    if (code == 2) { hady(); }
   });
 };
+
 setInterval(function() {
   console.clear();
 }, 3600000); 
