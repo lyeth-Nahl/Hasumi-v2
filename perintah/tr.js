509dc7ef22c16b6bd031ce76bed89ed8e18efd9b
Hady Zen <149669803+HadyZen@users.noreply.github.com>
2025-01-01 22:09:27 +0700

    Update tr.js

--- perintah/tr.js
+++ perintah/tr.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 module.exports ={ 
 config: { 
   nama: "tr",
@@ -6,16 +7,17 @@ config: {
   kuldown: 10,
   tutor: "<bahasa> <teks>"
 }, 
-
-Alya: async function ({ api, event, args }) { 
-  const axios = require('axios');
+bahasa: { 
+  id: { gada: "Kamu belum memasukkan code bahasa atau teksnya." }
+}, 
+Alya: async function ({ api, event, args, bhs }) { 
      if (args.join(' ')) { 
-      const bhs = args[0];
+      const basa = args[0];
       const hady = args.slice(1).join(' ');
-      const respon = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${bhs}&dt=t&q=${encodeURIComponent(hady)}`);
+      const respon = await axios.get(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${basa}&dt=t&q=${encodeURIComponent(hady)}`);
      api.sendMessage(`# 𝗧𝗲𝗿𝗷𝗲𝗺𝗮𝗵𝗮𝗻\n\n${respon.data[0].map(item => item[0]).join('')}`, event.threadID, event.messageID);
   } else {
-    return api.sendMessage("Masukkan pesan nya bodo", event.threadID, event.messageID);
+    return api.sendMessage(bhs('gada'), event.threadID, event.messageID);
   }
 }
 }
