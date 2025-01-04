f35ec1e475c47ef6057564b5e2ed8e5f625c4dd0
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-01-03 21:26:50 -0800

    Update admin.js
    Signed-off-by: MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>

--- perintah/admin.js
+++ perintah/admin.js
@@ -7,12 +7,41 @@ module.exports = {
     tutor: "<list/add/del>"
   },
   Alya: async function({ api, event, args }) {
+    // edit by Range
     const fs = require('fs');
     const noah = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
     switch (args[0]) {
       case 'list':
-        api.sendMessage(noah.admin.join('\n'), event.threadID, event.messageID);
+            try {
+          if (!Array.isArray(noah.admin) || noah.admin.length === 0) {
+            return api.sendMessage("Tidak ada admin yang terdaftar.", event.threadID);
+          }
+          
+          const mentions = [];
+          let nama = '';
+
+          for (let adminID of noah.admin) {
+            const userInfo = await api.getUserInfo(adminID);
+            if (userInfo && userInfo[adminID] && userInfo[adminID].name) {
+              const name = userInfo[adminID].name;
+              mentions.push({
+                id: adminID,
+                tag: name
+              });
+              nama += name + '\n';
+            } else {
+              console.log(`Nama pengguna dengan ID ${adminID} tidak ditemukan.`);
+            }
+          }
+
+          api.sendMessage({
+            body: "𝓛𝓲𝓼𝓽 𝓐𝓭𝓶𝓲𝓷\n" + `@${nama}`, 
+            mentions: mentions
+          }, event.threadID);
+        } catch (error) {
+          console.error(error);
+        }
         break;
       case 'add':
         if (args.length < 2) return api.sendMessage('Masukkan nama admin!', event.threadID, event.messageID);
