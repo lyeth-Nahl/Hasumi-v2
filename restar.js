af1f34ded25cf94b5993f3727d15873f6aa3c0c3
MaskStarmoon <starmoon20240@gmail.com>
2025-01-03 18:02:19 -0800

    Update and Fix bug

--- restar.js
+++
@@ -1,8 +0,0 @@
-/* HADY ZEN'IN */
-
-const { logo } = require('./hady-zen/log');
-
-process.on('SIGINT', function() {
-  console.log(logo.info + 'Alya diperintahkan berhenti...');
-  process.exit();
-});
