0387af5173a962c6876417c94c62764804e276bd
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-01-03 18:32:47 -0800

    Add files via upload
    Signed-off-by: MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>

---
+++ perintah/syntora.js
@@ -0,0 +1,58 @@
+// const awalan = ["Syntora", "Syn"];
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports = {
+config: { 
+  nama: "syntora",
+  penulis: "Range", 
+  kuldown: 10,
+  peran: 0,
+  tutor: "<pertanyaan>"
+}, 
+
+Alya: async function (api, event, args) { 
+  // edit by Range
+  const text = args.join(' ');
+  const userID = event.senderID;
+  const memo = "./perintah/Memory-Syn/SynMemo.json";
+  const isAdmin = global.config.admin.includes(userID);
+
+  if (!fs.existsSync("./perintah/Memory-Syn")) return fs.mkdirSync("./perintah/Memory-Syn", { recursive: true });
+  if (!fs.existsSync(memo)) return fs.writeFileSync(memo, JSON.stringify([]));
+
+  let memoData = JSON.parse(fs.readFileSync(memo, 'utf8'));
+  if (isAdmin) {
+      if (text.toLowerCase() === "delete all") {
+        memoData = {};
+        fs.writeFileSync(memo, JSON.stringify(memoData, null, 2));
+        return api.sendMessage("Semua riwayat percakapan berhasil dihapus!", event.threadID, event.messageID);
+      }
+      
+      if (text.startsWith("delete ")) {
+        const targetID = text.split(" ")[1];
+        if (memoData[targetID]) {
+          delete memoData[targetID];
+          fs.writeFileSync(memo, JSON.stringify(memoData, null, 2));
+          return api.sendMessage(`Riwayat percakapan untuk ID ${targetID} berhasil dihapus!`, event.threadID, event.messageID);
+        } else {
+          return api.sendMessage(`Tidak ditemukan riwayat untuk ID ${targetID}.`, event.threadID, event.messageID);
+        }
+      }
+    }
+  if (text) {
+    let previousText = memoData[userID]?.text || "No Input";
+      memoData[userID] = { text: text };
+      fs.writeFileSync(memo, JSON.stringify(memoData, null, 2));
+      const hady = `Nama kamu adalah Syntora Dynamix, Kamu adalah artificial intelligence bot yang ramah dan baik hati, Kamu di ciptakan oleh tim pengembang dari kolaborasi nebula dan shenix. User input: ${text}, User Previous Input: ${previousText}`;
+      const { data } = await axios.get(`https://api-rangestudio.vercel.app/api/gemini?text=${encodeURIComponent(hady)}`);
+      if (data.answer) {
+        return api.sendMessage(data.answer, event.threadID, event.messageID);
+      } else {
+        return api.sendMessage("Tidak dapat merespon!", event.threadID, event.messageID);
+      }
+  } else {
+    return api.sendMessage("Masukkan pesan nya dulu...", event.threadID, event.messageID);
+  }
+ }
+};
