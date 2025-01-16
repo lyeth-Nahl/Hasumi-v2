38ab99002422cbbae97396469d529653fa0c265c
MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>
2025-01-15 18:37:59 -0800

    Update status.js
    Signed-off-by: MaskStarmoon <150095487+MaskStarmoon@users.noreply.github.com>

--- perintah/status.js
+++ perintah/status.js
@@ -9,7 +9,6 @@ module.exports = {
 
   Alya: async function ({ api, event, getData }) {
     const senderID = event.senderID; 
-    const user = await api.getUserInfo(senderID);
     try {
       const data = await getData(senderID);  
 
@@ -17,18 +16,18 @@ module.exports = {
         api.sendMessage("Data tidak ditemukan.", event.threadID);
         return;
       }
-
+      const name = data.name;
       const fakeID = data.fakeID || 'Tidak ditemukan';
       const exp = data.exp || 0;
       const money = data.money || 0;
 
-      const statusMessage = `*Status Pengguna:*\nName: ${user[senderID].name}\nID: ${fakeID}\nExp: ${exp}\nMoney: ${money}`;
+      const statusMessage = `*Status Pengguna:*\nName: ${name}\nID: ${fakeID}\nExp: ${exp}\nMoney: ${money}`;
 
       api.sendMessage(statusMessage, event.threadID, event.messageID);
       
     } catch (error) {
       console.error("Gagal mengambil data:", error);
-      api.sendMessage("Terjadi kesalahan saat mengambil data status.", event.threadID, event.messageID);
+      api.sendMessage("Terjadi kesalahan saat mengambil data status.", event.threadID);
     }
   }
 };
